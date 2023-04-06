44c3174369c065fd45f3060a4bf75fdef7488482
임서후 <imseohu2@gmail.com>
2023-04-06 06:28:12 +0000

    연습

---
+++ src/42.c
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+int main()
+{
+	
+	printf("실행");
+	return 0;
+}
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+	printf("Hello, goorm!\n");
+	printf(" 컴퓨터과 1학년 3반\n");
+	printf(" 임서후\n");
+	
+	
+	return 0;
+}
