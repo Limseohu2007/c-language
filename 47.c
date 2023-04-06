576e819e518f2aabf240f8a8a60da7ebb1b8ba97
임서후 <imseohu2@gmail.com>
2023-04-06 05:30:14 +0000

    연습

---
+++ 47.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int num = 1;
+	int num2 = 2;
+	int num3 = 3;
+	int num4 = 4;
+	int num5 = 5;
+	printf("%d\n", num);
+	printf("%d\n", num2);
+	printf("%d\n", num3);
+	printf("%d\n", num4);
+	printf("%d", num5);
+	return 0;
+}
